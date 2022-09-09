04804620dc401d5fcb54d56c54170e34291671f4
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-09-09 03:07:48 -0700

    hgh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+ **/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
 Footer
-© 2022 GitHub, Inc.
+© 2022 GitHub, Inci.
