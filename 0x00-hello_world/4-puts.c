c7fcb6ec32727a2c96932e53a89827adfc361b89
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-09-08 21:58:40 -0700

    tff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,8 @@
-/*
- *
- *  * File: 4-puts.c
- *
- *   * Auth: Brennan D Baraban <375@holbertonschool.com>
- *
- *    */
-
-
-
 #include <stdio.h>
 
 
 
-/**
+/*
  *
  *  * main - Prints "Programming is like building a multilingual
  *
