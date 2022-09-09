321c2480f48a55b6d36d6075a9fbae5540cec273
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-09-08 22:06:59 -0700

    yyyy

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,37 +1,9 @@
-/*
- *
- *  * File: 5-printf.c
- *
- *   * Auth: Brennan D Baraban
- *
- *    */
-
-
-
-#include <stdio.h>
-
-
-
-/**
- *
- *  * main - Prints "with proper grammar, but the outcome is
- *
- *   *                a piece of art," followed by a new line.
- *
- *    *
- *
- *     * Return: Always 0.
- *
- *      */
-
-int main(void)
-
-{
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-			return (0);
-
-}
+ 1   // Fig. 2.1: fig02_01.c
+  2   // A first program in C.
+   3   #include <stdio.h>
+    4
+     5   // function main begins program execution
+      6   int main( void )
+	 7   {
+		  8      printf( "proper grammar, but the outcome is a piece of art,\n" );
+		   9   } // end function main
