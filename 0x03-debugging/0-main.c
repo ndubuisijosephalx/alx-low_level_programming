582b05499f6e02221685e7566016ccde0ac5ff96
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-09-14 12:24:24 -0700

    fdffgg

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "../main.h"
+
+
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ *
+ *
+ * Return: Always 0.
+ *
+ **/
+int main(void)
+
+{
+
+int i;
+i = 98;
+positive_or_negative(i);
+return (0);
+
+}
