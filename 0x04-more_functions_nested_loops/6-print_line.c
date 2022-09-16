1ca691899f28dcdfedcacb5edf28ec1789677771
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-09-15 22:47:46 -0700

    gfdd

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,13 +2,16 @@
 
 /**
  * print_line - a function that draws a straight line in the terminal
+
  * @n: input number of times to print '_'
+
  * Return: a straight line
  */
 
 void print_line(int n)
 {
 int co;
+
 if (n <= 0)
 {
 _putchar('\n');
@@ -20,4 +23,5 @@ for (co = 1; co <= n; co++)
 _putchar('_');
 }
 _putchar('\n');
+}
 }
