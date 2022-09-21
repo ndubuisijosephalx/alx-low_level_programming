1048179788d5d5b937e46a924a0b1b584dffa055
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-09-21 12:41:47 -0700

    uguiyg

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,5 @@
 #include <string.h>
-#include <stdio.h>  
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,7 +9,9 @@
 void print_rev(char *s)
 {
 int len = strlen(s);
-while (len--);
+
+while (len--)
 putchar(*(s + len));
+
 putchar(10);
 }
