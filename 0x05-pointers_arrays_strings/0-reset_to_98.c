e584264bcd5d1a859a5a294f2714110525290491
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-09-21 06:31:57 -0700

    hjfkjeke

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-include main.h                                                                                                                                                        
+#include main.h                                                                                                                                                        
                                                                                                                                                                       
 /**                                                                                                                                                                   
  * C program to use pointer function                                                                                                                                  
