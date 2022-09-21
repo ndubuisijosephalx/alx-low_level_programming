c29a6a02854196202acb4249d533ecdded1ec37a
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-09-21 10:57:06 -0700

    fdsff

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,8 +2,9 @@
 
 /**
  * _strcpy - Copies a string pointed to by @src, including the
- *terminating null byte, to a buffer pointed to by @dest.
+ * terminating null byte, to a buffer pointed to by @dest.
  * @dest: A buffer to copy the string to.
+ *
  * @src: The source string to copy.
  *
  * Return: A pointer to the destination string @dest.
@@ -12,6 +13,10 @@
 char *_strcpy(char *dest, const char *src)
 
 {
+
+	int index = 0;
+
+
 
 	while (src[index])
 
@@ -26,4 +31,5 @@ char *_strcpy(char *dest, const char *src)
 
 
 	return (dest);
+
 }
