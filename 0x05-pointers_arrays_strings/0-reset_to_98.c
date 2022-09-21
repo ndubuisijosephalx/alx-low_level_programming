fd2b8ba319ba26730622a80442b16baf8ba1c5a4
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-09-21 06:25:12 -0700

    hfkj

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+include main.h                                                                                                                                                        
+                                                                                                                                                                      
+/**                                                                                                                                                                   
+ * C program to use pointer function                                                                                                                                  
+ *                                                                                                                                                                    
+ * main reset fuction                                                                                                                                                 
+ */                                                                                                                                                                   
+/*Main: function prototype*/                                                                                                                                          
+                                                                                                                                                                      
+void reset_to_98(int *n)                                                                                                                                              
+{                                                                                                                                                                     
+                                                                                                                                                                      
+*n = 98;                                                                                                                                                              
+                                                                                                                                                                      
+}
