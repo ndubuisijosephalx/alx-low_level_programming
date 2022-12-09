11784e1b7fc226093eaa54040f8b9ab0f3e11a76
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-12-08 23:39:05 -0800

    gfgg

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,16 +3,10 @@
 
 
 /**
-
 * dlistint_len - return number of elements in a linked dlistint_t list.
-
 * @h: head of the doubly linked list
-
 * Return: Returns the number of element
-
 */
-
-
 
 size_t dlistint_len(const dlistint_t *h)
 
