f576fe9e1c800fe09cbe797e982f05a268d40587
ndubuisijosephalx <ndubuisijoseph19@gmail.com>
2022-12-08 18:19:49 -0800

    ggg

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,10 @@
 #include "lists.h"
 
-
-
 /**
-
 * print_dlistint - prints all the elements of a dlistint_t list.
-
 * @h: head of the doubly linked list
-
 * Return: Returns the number of element
-
 */
-
-
 
 size_t print_dlistint(const dlistint_t *h)
 
